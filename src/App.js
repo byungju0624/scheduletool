832a76e2bb1321abec015461c0cee5f2467f0adc
byungju0624 <byungju0624@gmail.com>
2021-04-20 21:59:01 +0900

    todoinsert add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './components/todotemplate/todotemplate';
+import TodoInsert from './components/todoinsert/todoinsert';
 function App() {
-  return <TodoTemplate>Todo app create</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
 }
 
 export default App;
---
+++ src/components/todoinsert/todoinsert.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './todoinsert.scss';
+import { MdAdd } from 'react-icons/md';
+const TodoInsert = (props) => {
+  return (
+    <form className="todoinsert">
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
