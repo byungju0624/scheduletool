248ada8eda0688cde9704e30a1746ee11a9d48f5
byungju0624 <byungju0624@gmail.com>
2021-04-24 19:57:12 +0900

    .

--- src/components/todoinsert/todoinsert.jsx
+++ src/components/todoinsert/todoinsert.jsx
@@ -4,7 +4,7 @@ import { MdAdd } from 'react-icons/md';
 const TodoInsert = (props) => {
   return (
     <form className="todoinsert">
-      <input placeholder="할 일을 입력하세요" />
+      <input placeholder="할 일을 입력하세요!" />
       <button type="submit">
         <MdAdd />
       </button>
